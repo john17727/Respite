ace3f22626e321426340eba16c228680f84ff9e3
Juan Rincon <justanotherone17727@gmail.com>
2024-02-03 21:53:15 -0500

    Add Trip Screen.

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -2,18 +2,22 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import domain.model.Trip
 import presentation.`trip-list`.TripListScreen
+import presentation.trip.TripScreen
 
 @Composable
 fun App() {
     MaterialTheme {
-        TripListScreen(
-            listOf(
-                Trip(1, "Las Vegas", 30),
-                Trip(2, "Roswell", 30),
-                Trip(1, "El Paso", 30),
-            ),
-            {},
-            {}
+        TripScreen(
+            Trip(3, "El Paso", 30)
         )
+//        TripListScreen(
+//            listOf(
+//                Trip(1, "Las Vegas", 30),
+//                Trip(2, "Roswell", 30),
+//                Trip(3, "El Paso", 30),
+//            ),
+//            {},
+//            {}
+//        )
     }
 }
