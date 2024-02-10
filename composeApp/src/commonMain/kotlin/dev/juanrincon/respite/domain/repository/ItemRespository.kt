947676daa081ecbe1ad9a02097fad7df4879b59a
Juan Rincon <justanotherone17727@gmail.com>
2024-02-10 18:19:16 -0500

    Add Luggage Repository implementation.

--- composeApp/src/commonMain/kotlin/dev/juanrincon/respite/domain/repository/ItemRespository.kt
+++ composeApp/src/commonMain/kotlin/dev/juanrincon/respite/domain/repository/ItemRespository.kt
@@ -4,11 +4,11 @@ import dev.juanrincon.respite.domain.model.Item
 
 interface ItemRespository {
     
-    suspend fun create(newItem: Item): Result<Unit>
+    suspend fun create(name: String, categoryId: Int): Result<Unit>
 
     suspend fun read(): Result<List<Item>>
-    
-    suspend fun update(item: Item): Result<Unit>
+
+    suspend fun update(id: Int, name: String, categoryId: Int): Result<Unit>
     
     suspend fun delete(id: Int): Result<Unit>
 }
