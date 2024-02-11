3690aa0ccee820e9a15435147f9fce43a4471698
Juan Rincon <justanotherone17727@gmail.com>
2024-02-10 21:30:24 -0500

    Add list animation.

--- composeApp/src/commonMain/kotlin/dev/juanrincon/respite/presentation/luggage/LuggageUI.kt
+++ composeApp/src/commonMain/kotlin/dev/juanrincon/respite/presentation/luggage/LuggageUI.kt
@@ -1,5 +1,7 @@
 package dev.juanrincon.respite.presentation.luggage
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -18,11 +20,13 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import dev.juanrincon.respite.domain.model.Category
+import dev.juanrincon.respite.presentation.animations.fadeInFadeOut
 import dev.juanrincon.respite.presentation.components.BannerAlignment
 import dev.juanrincon.respite.presentation.components.EditingLuggageItem
 import dev.juanrincon.respite.presentation.components.UserLuggageItem
 import dev.juanrincon.respite.presentation.components.VerticalBanner
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun LuggageUI(
     luggage: List<LuggageItem>,
@@ -44,26 +48,32 @@ fun LuggageUI(
                 contentPadding = PaddingValues(top = 16.dp, start = 24.dp),
                 modifier = Modifier.fillMaxHeight().weight(1f)
             ) {
-                items(luggage, { item -> item.id }, { item -> item::class }) { item ->
-                    when (item) {
-                        is LuggageItem.CreatingItem -> Text(item.item.name)
-                        is LuggageItem.EditingItem -> EditingLuggageItem(
-                            item = item.item,
-                            categories = categories,
-                            onCancel = onEditCancel,
-                            onSave = onEditSave,
-                            focusRequester = focusRequester,
-                            borderColor = Color(0xFFFFD55F),
-                            contentColor = Color(0xFF684633),
-                        )
+                items(luggage, { item -> item.id }, { item -> item::class }) { luggageItem ->
+                    AnimatedContent(
+                        luggageItem,
+                        transitionSpec = ::fadeInFadeOut,
+                        modifier = Modifier.animateItemPlacement()
+                    ) { item ->
+                        when (item) {
+                            is LuggageItem.CreatingItem -> Text(item.item.name)
+                            is LuggageItem.EditingItem -> EditingLuggageItem(
+                                item = item.item,
+                                categories = categories,
+                                onCancel = onEditCancel,
+                                onSave = onEditSave,
+                                focusRequester = focusRequester,
+                                borderColor = Color(0xFFFFD55F),
+                                contentColor = Color(0xFF684633),
+                            )
 
-                        is LuggageItem.UserItem -> UserLuggageItem(
-                            item = item.item,
-                            borderColor = Color(0xFFFFD55F),
-                            contentColor = Color(0xFF684633),
-                            onEditClick = onEditClick,
-                            onDeleteClick = onDeleteClick
-                        )
+                            is LuggageItem.UserItem -> UserLuggageItem(
+                                item = item.item,
+                                borderColor = Color(0xFFFFD55F),
+                                contentColor = Color(0xFF684633),
+                                onEditClick = onEditClick,
+                                onDeleteClick = onDeleteClick
+                            )
+                        }
                     }
                 }
             }
