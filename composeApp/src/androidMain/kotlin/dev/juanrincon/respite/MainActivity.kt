db72e696277c4d6782c096829a9e0db81b444e37
Juan Rincon <justanotherone17727@gmail.com>
2024-02-13 20:00:31 -0500

    Make Android fullscreen.

--- composeApp/src/androidMain/kotlin/dev/juanrincon/respite/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/dev/juanrincon/respite/MainActivity.kt
@@ -3,10 +3,13 @@ package dev.juanrincon.respite
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
 
         setContent {
             App()
