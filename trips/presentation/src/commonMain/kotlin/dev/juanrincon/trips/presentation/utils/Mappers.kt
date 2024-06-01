12402bf559f90b39cd285a6e85bc4bf684228234
Juan Rincon <justanotherone17727@gmail.com>
2024-06-01 17:02:55 -0400

    Update Gradle Versions

--- trips/presentation/src/commonMain/kotlin/dev/juanrincon/trips/presentation/utils/Mappers.kt
+++ trips/presentation/src/commonMain/kotlin/dev/juanrincon/trips/presentation/utils/Mappers.kt
@@ -8,7 +8,7 @@ import dev.juanrincon.trips.presentation.models.UITrip
 import dev.juanrincon.trips.presentation.models.UITripItem
 import dev.juanrincon.trips.presentation.models.UITripStatus
 
-inline fun Trip.toUIModel() = UITrip(
+fun Trip.toUIModel() = UITrip(
     this.id,
     this.name.getCityAbbreviation(),
     this.status.toUIModel(),
@@ -16,20 +16,20 @@ inline fun Trip.toUIModel() = UITrip(
     this.items.map { it.toUIModel() }
 )
 
-inline fun TripItem.toUIModel() = UITripItem(
+fun TripItem.toUIModel() = UITripItem(
     this.id,
     this.name,
     this.category,
     this.total
 )
 
-inline fun TripStatus.toUIModel() = when (this) {
+fun TripStatus.toUIModel() = when (this) {
     TripStatus.Destination -> UITripStatus.Destination
     TripStatus.PackingDestination -> UITripStatus.PackingDestination
     TripStatus.PackingNextDestination -> UITripStatus.PackingNextDestination
 }
 
-inline fun UITrip.toDomainModel() = Trip(
+fun UITrip.toDomainModel() = Trip(
     this.id,
     this.name,
     this.status.toDomainModel(),
@@ -37,13 +37,13 @@ inline fun UITrip.toDomainModel() = Trip(
     this.items.map { it.toDomainModel() }
 )
 
-inline fun UITripStatus.toDomainModel() = when (this) {
+fun UITripStatus.toDomainModel() = when (this) {
     UITripStatus.Destination -> TripStatus.Destination
     UITripStatus.PackingDestination -> TripStatus.PackingDestination
     UITripStatus.PackingNextDestination -> TripStatus.PackingNextDestination
 }
 
-inline fun UITripItem.toDomainModel() = TripItem(
+fun UITripItem.toDomainModel() = TripItem(
     this.id,
     this.name,
     this.category,
